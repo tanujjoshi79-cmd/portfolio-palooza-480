d9557708ead9bf9ed241cdc70c6bb539cff349c2
tanujjoshi79-cmd <tanujjoshi79@gmail.com>
2026-09-04 22:07:33 -0700

    Fix favicon loading and bust browser cache

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,7 +95,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "icon", href: "/favicon.svg?v=2", type: "image/svg+xml" },
+      { rel: "shortcut icon", href: "/favicon.svg?v=2", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
